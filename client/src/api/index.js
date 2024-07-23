4a5816d3c8e9bd02584ba49e1a590775746adbbb
Rohit Sharma <rohitsharma63693@gmail.com>
2024-07-23 18:52:53 +0530

    update base url for api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://artist-hu-bhai.onrender.com/api/",
 });
 
-export const GetPosts = async () => await API.get("/api/post/");
-export const createPost = async (data) => await API.post("/api/post/", data);
+export const GetPosts = async () => await API.get("/post/");
+export const createPost = async (data) => await API.post("/post/", data);
 export const GenerateAIImage = async (data) =>
   await API.post("/api/generateImage/", data);
